ce5fe8aa6554a7c0659bb8cf5216af045b44dd33
Akiko Yamasaki <paonpaon@gmail.com>
2023-04-24 21:07:06 +1000

    user sign in and log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :fetch_user
+    private
+    def fetch_user
+        @current_user = User.find_by :id => session[:user_id]
+        session[:user_id] = nil unless @current_user.present? #long out non-existent users
+    end
+
+    #only logged in user can have an access to certain people
+    def check_for_login
+        redirect_to login_path unless @current_user.present?
+    end
+
+    # def check_for_admin
+    #     redirect_to login_path unless(@current_user.present? && @current_user.admin?)
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'session/new'
   # get 'users/new'
   # get 'pages/home'
   root :to => 'pages#home'
@@ -6,4 +7,7 @@ Rails.application.routes.draw do
   resources :works
   get '/new_user', to: "users#new"
   post '/new_user', to: "users#create"
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
